d4a19eb0bc6783b1d7571908b6925c0b10052e78
Yoan Fahrni <yoan.fahrni@divtec.ch>
2025-10-28 14:29:00 +0100

    Recherche dynamique

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,21 +1,38 @@
 <template>
   <h1>Accueil</h1>
-  <div v-for="(pays, index) in store.resources" :key="index">
-    <img :alt="'Drapeau du pays : ' + pays.name.common" :src="pays.flags.svg">
-    <h2>{{ pays.name.common }}</h2>
-    <p>{{ pays.capital[0] }}</p>
-  </div>
+  <v-text-field v-model="nom" placeholder="Cherchez un pays" type="text" @input="trierEnFonctionSaisie()" />
+  <v-container>
+    <v-row>
+      <v-col v-for="(pays, index) in lesPays"
+             :key="index"
+             cols="12"
+             md="4"
+             sm="6"
+      >
+        <v-img :alt="'Drapeau du pays : ' + pays.name.common" class="img" :src="pays.flags.svg" />
+        <h2>{{ pays.name.common }}</h2>
+        <p>{{ pays.capital[0] }}</p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
+  // import { ref } from 'vue'
   import { useAppStore } from '@/stores/app.js'
 
+  const nom = ref('')
+  const lesPays = ref([])
   const store = useAppStore()
-</script>
 
-<style>
-  img {
-    width: 200px;
-    height: auto;
+  function trierEnFonctionSaisie () {
+    lesPays.value = []
+
+    for (const pays of store.resources) {
+      if (pays.name.common.toLowerCase().includes(nom.value.toLowerCase()) || nom.value === '') {
+        lesPays.value.push(pays)
+      }
+    }
   }
-</style>
+  trierEnFonctionSaisie()
+</script>
